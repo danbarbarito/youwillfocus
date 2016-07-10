ae411cb6d2fe7fbc929b3e3c1d2b90ba8adda3bd
Dan Barbarito <danielbarbarito@mail.adelphi.edu>
2016-07-10 05:25:45 -0400

    Pushed using Fabric

--- fabfile.py
+++ fabfile.py
@@ -29,7 +29,5 @@ def deploy():
         if run("test -d %s" % code_dir).failed:
             run("git clone git@github.com:danielbarbarito/youwillfocus.git %s" % code_dir)
     with cd(code_dir):
-        with settings(warn_only=True):
-            kill_running = run("killall youwillfocus")
         run("git pull origin master")
         run("sudo service youwillfocus restart")
