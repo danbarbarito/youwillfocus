507c31c4503639119d41829b0c256023978bfdea
Dan Barbarito <danielbarbarito@mail.adelphi.edu>
2016-07-10 20:46:16 -0400

    Pushed using Fabric

--- youwillfocus.go
+++ youwillfocus.go
@@ -1,59 +1,71 @@
 package main
 
 import (
-		"fmt"
-		"flag"
-		"net/http"
-		"time"
+	"flag"
+	"fmt"
+	"net/http"
+	"time"
+	os_user "os/user"
 
-		"youwillfocus/app/common"
-		"youwillfocus/app/home"
-		"youwillfocus/app/user"
+	"youwillfocus/app/common"
+	"youwillfocus/app/home"
+	"youwillfocus/app/user"
 
-		"github.com/golang/glog"
-		"github.com/gorilla/mux"
+	"github.com/golang/glog"
+	"github.com/gorilla/mux"
 )
 
 func main() {
-		flag.Parse()
-		defer glog.Flush()
+	flag.Parse()
+	defer glog.Flush()
 
-		router := mux.NewRouter()
-		http.Handle("/", httpInterceptor(router))
+	router := mux.NewRouter()
+	http.Handle("/", httpInterceptor(router))
 
-		router.HandleFunc("/", home.GetHomePage).Methods("GET")
-		router.HandleFunc("/user{_:/?}", user.GetHomePage).Methods("GET")
+	router.HandleFunc("/", home.GetHomePage).Methods("GET")
+	router.HandleFunc("/user{_:/?}", user.GetHomePage).Methods("GET")
 
-		router.HandleFunc("/user/view/{id:[0-9]+}", user.GetViewPage).Methods("GET")
-		router.HandleFunc("/user/{id:[0-9]+}", user.GetViewPage).Methods("GET")
+	router.HandleFunc("/user/view/{id:[0-9]+}", user.GetViewPage).Methods("GET")
+	router.HandleFunc("/user/{id:[0-9]+}", user.GetViewPage).Methods("GET")
 
-		fileServer := http.StripPrefix("/static/", http.FileServer(http.Dir("static")))
-		http.Handle("/static/", fileServer)
+	fileServer := http.StripPrefix("/static/", http.FileServer(http.Dir("static")))
+	http.Handle("/static/", fileServer)
 
-		err := http.ListenAndServe(":80", nil)
+	// Run on port 80 if running as root, 8080 if not
 
-		if err != nil {
-				fmt.Println(err)
-		}
+	runningAs, _ := os_user.Current()
+
+	port := ":8080"
+
+	switch runningAs.Username {
+	case "root":
+		port = ":80"
+	}
+
+	err := http.ListenAndServe(port, nil)
+
+	if err != nil {
+		fmt.Println(err)
+	}
 }
 
 func httpInterceptor(router http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-				startTime := time.Now()
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		startTime := time.Now()
 
-				router.ServeHTTP(w, req)
+		router.ServeHTTP(w, req)
 
-				finishTime := time.Now()
-				elapsedTime := finishTime.Sub(startTime)
+		finishTime := time.Now()
+		elapsedTime := finishTime.Sub(startTime)
 
-				switch req.Method {
-				case "GET":
-						// We may not always want to StatusOK, but for the sake of
-						// this example we will
-						common.LogAccess(w, req, elapsedTime)
-				case "POST":
-						// here we might use http.StatusCreated
-				}
+		switch req.Method {
+		case "GET":
+			// We may not always want to StatusOK, but for the sake of
+			// this example we will
+			common.LogAccess(w, req, elapsedTime)
+		case "POST":
+			// here we might use http.StatusCreated
+		}
 
-		})
+	})
 }
